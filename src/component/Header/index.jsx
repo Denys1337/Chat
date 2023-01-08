9d7f60f7bdccf4a413feab16a36a3ead0b802cd4
Denys1337 <bondarets1488@gmail.com>
2023-01-08 04:09:54 +0200

    add dropdown to profile

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import UserProfile from '../UserProfile';
 //component
-import Login from '../Login/Login';
+
 // styles
 import styles from './Header.module.scss';
 
 const Header = () => {
-  const user = useSelector(state => state.user.user);
+  
+
   return (
     <div className={styles.wrapper}>
       <h1>Chat-webSocket</h1>
-      {user ? <img src={user.photoURL} alt=''/> : <Login/>}
+      <div className={styles.wrapper__profile}>
+       <UserProfile/>
+      </div>
     </div>
   )
 }
--- src/component/UserProfile/index.jsx
+++ src/component/UserProfile/index.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+// components
+import { PopupMenu } from "react-simple-widgets";
+import Login from '../Login/Login';
+//styles
+import styles from "./UserProfile.module.scss";
 
 const UserProfile = () => {
+  const user = useSelector(state => state.user.user);
   return (
-    <div>UserProfile</div>
+    <div>
+      <PopupMenu>
+        {user ?
+          <button className={styles.buttonProfile}>
+            <small> My Account</small>
+          </button>
+          : ''
+        }
+
+          <div className={styles.wrapperModal}>
+            <div>
+              <div className={styles.wrapperModal__circleAvatar}>
+                {user ? <img src={user.photoURL} alt='' /> : <Login />}
+              </div>
+
+            <h5 className={styles.wrapperModal__nameUser}>{user ? user.displayName : ''}</h5>
+              <p className={styles.wrapperModal__emailUser}>{user ? user.email : ''}</p>
+
+              <hr />
+
+              <p
+                className={styles.wrapperModal__titleRole}
+                style={{ color: "#bebebe", fontWeight: "bold", fontSize: 12 }}
+              >
+                ROLES
+              </p>
+              <p style={{ fontSize: 14 }} className={styles.wrapperModal__ContentRole}>
+                {["Submitter", "Project manager", "Change control board"].join(
+                  ", "
+                )}
+              </p>
+              <div
+                className={styles.wrapperModal__groupList}
+                style={{ margin: "0 -24px 0" }}
+              >
+                <button className={styles.wrapperModal__groupList__item}>
+                  <small>Change Requests</small>
+                </button>
+                <button className={styles.wrapperModal__groupList__item}>
+                  <small>Pending Requests</small>
+                </button>
+                <button className={styles.wrapperModal__groupList__item}>
+                  <small>Other Requests</small>
+                </button>
+              </div>
+
+              <div className={styles.wrapperModal__buttonContainer}>
+                <button className={styles.wrapperModal__buttonContainer__logout}>
+                  <small>Logout</small>
+                </button>
+              </div>
+            </div>
+          </div>
+        </PopupMenu>
+      </div>
   )
 }
 
